f5ab90311d528716d0ac52220c2df5ba7a53484f
buxin <buxin@yahoo.cn>
2018-12-04 15:57:35 +0800

    core

--- modules/Scene/SceneMediator.ts
+++ modules/Scene/SceneMediator.ts
@@ -12,7 +12,7 @@ import {BasicSceneEntity} from "../../base/BasicSceneEntity";
 import {Log} from "../../Log";
 
 export class SceneMediator extends MediatorBase {
-    private hasRgistHandler: boolean = false;
+    private hasRegisterHandler: boolean = false;
     private flowManager: FlowManager;
 
     // private sceneLoader: SceneLoader;
@@ -25,13 +25,36 @@ export class SceneMediator extends MediatorBase {
         return this.viewComponent as SceneView;
     }
 
+    private _move_graphics: Phaser.Graphics;
+
+    private get move_graphics(): Phaser.Graphics {
+        if (this._move_graphics == null) {
+            this._move_graphics = Globals.game.make.graphics();
+            this._move_graphics.clear();
+            this._move_graphics.lineStyle(2, 0x00ff00, 1);
+            Globals.LayerManager.sceneLayer.add(this._move_graphics);
+        }
+        return this._move_graphics;
+    }
+
+    private _stop_graphics: Phaser.Graphics;
+
+    private get stop_graphics(): Phaser.Graphics {
+        if (this._stop_graphics == null) {
+            this._stop_graphics = Globals.game.make.graphics();
+            this._stop_graphics.lineStyle(2, 0xff0000, 1);
+            this.view.addChild(this._stop_graphics);
+        }
+        return this._stop_graphics;
+    }
+
     public onRegister(): void {
         super.onRegister();
         this.onLoginOk();
     }
 
     public onRemove(): void {
-        this.unRegistSceneListenerHandler();
+        this.unRegisterSceneListenerHandler();
         if (this.flowManager)
             this.flowManager.dispose();
         this.flowManager = null;
@@ -39,19 +62,19 @@ export class SceneMediator extends MediatorBase {
     }
 
     //server handler
-    public registSceneListenerHandler(): void {
-        if (!this.hasRgistHandler) {
+    public registerSceneListenerHandler(): void {
+        if (!this.hasRegisterHandler) {
             Globals.MessageCenter.on(MessageType.SCENE_MOVE_TO, this.moveToHandle, this);
             Globals.MessageCenter.on(MessageType.SCENE_MOVE_STOP, this.moveStopHandle, this);
-            this.hasRgistHandler = true;
+            this.hasRegisterHandler = true;
         }
     }
 
-    public unRegistSceneListenerHandler(): void {
-        if (this.hasRgistHandler) {
+    public unRegisterSceneListenerHandler(): void {
+        if (this.hasRegisterHandler) {
             Globals.MessageCenter.cancel(MessageType.SCENE_MOVE_TO, this.moveToHandle, this);
             Globals.MessageCenter.cancel(MessageType.SCENE_MOVE_STOP, this.moveStopHandle, this);
-            this.hasRgistHandler = false;
+            this.hasRegisterHandler = false;
         }
     }
 
@@ -77,14 +100,21 @@ export class SceneMediator extends MediatorBase {
         Globals.MessageCenter.emit(MessageType.SCENE_INITIALIZED);
     }
 
+    private onDraw(graphics: Phaser.Graphics, ox: number, oy: number, x: number, y: number) {
+        graphics.moveTo(ox, oy);
+        graphics.lineTo(x, y);
+    }
+
     private moveToHandle(moveData: op_client.IMoveData[]): void {
         let imove: op_client.IMoveData;
         let entity: BasicSceneEntity;
         for (let i = 0; i < moveData.length; i++) {
             imove = moveData[i];
             entity = this.view.getSceneElement(imove.moveObjectId);
-            if (this.view.currentSelfPlayer.uid === imove.moveObjectId)
-                Log.trace("[收到] <--> ", imove.direction, imove.timeSpan, imove.destinationPoint3f.x, imove.destinationPoint3f.y);
+            if (this.view.currentSelfPlayer.uid === imove.moveObjectId) {
+                this.onDraw(this.move_graphics, this.view.currentSelfPlayer.ox, this.view.currentSelfPlayer.oy, imove.destinationPoint3f.x, imove.destinationPoint3f.y);
+                // Log.trace("[收到] <--> ", imove.direction, imove.timeSpan, imove.destinationPoint3f.x, imove.destinationPoint3f.y);
+            }
             if (entity)
                 entity.moveToTarget(imove);
         }
@@ -96,10 +126,12 @@ export class SceneMediator extends MediatorBase {
         for (let i = 0; i < posData.length; i++) {
             imove = posData[i];
             entity = this.view.getSceneElement(imove.moveObjectId);
-            if (this.view.currentSelfPlayer.uid === imove.moveObjectId)
-                Log.trace("[收到] <--> ", imove.direction, imove.destinationPoint3f.x, imove.destinationPoint3f.y);
-            // if (entity)
-                // entity.setPosition(imove.destinationPoint3f.x, imove.destinationPoint3f.y, imove.destinationPoint3f.z);
+            if (this.view.currentSelfPlayer.uid === imove.moveObjectId) {
+                this.onDraw(this.stop_graphics, this.view.currentSelfPlayer.ox, this.view.currentSelfPlayer.oy, imove.destinationPoint3f.x, imove.destinationPoint3f.y);
+                // Log.trace("[收到-停下] <--> ", imove.direction, imove.destinationPoint3f.x, imove.destinationPoint3f.y);
+            }
+            if (entity)
+                entity.setPosition(imove.destinationPoint3f.x, imove.destinationPoint3f.y, imove.destinationPoint3f.z);
         }
     }
 
@@ -120,7 +152,7 @@ export class SceneMediator extends MediatorBase {
         //mapScene
         // this.sceneLoader.changedToMap(Globals.DataCenter.PlayerData.mainPlayerInfo.mapId);
 
-        this.registSceneListenerHandler();
+        this.registerSceneListenerHandler();
 
         this.changedToMapSceneCompleteHandler(Globals.DataCenter.SceneData.mapInfo);
     }
