325e874d61715f61c6a345eb2b2c8683df03fdb2
buxin <buxin@yahoo.cn>
2018-12-23 15:10:12 +0800

    CORE

--- base/module/base/ModuleType.ts
+++ base/module/base/ModuleType.ts
@@ -1,7 +1,9 @@
 export class ModuleTypeEnum {
     public static CONTROL = "Control"; // 模块控制模块
-    public static MAINMENU = "MainMenu"; // 商店
+    public static MAINMENU = "MainMenu"; // 主界面菜单
     public static SCENE = "Scene"; // 场景
-    public static BAG = "Bag"; // 商店
+    public static BAG = "Bag"; // 背包
     public static STORE = "Store"; // 商店
+    public static CHAT = "Chat"; // 聊天
+
 }
