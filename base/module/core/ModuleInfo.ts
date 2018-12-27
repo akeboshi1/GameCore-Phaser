402bddb5b9a9f1abee77e7aa747cdf8d3f187aef
buxin <buxin@yahoo.cn>
2018-12-27 10:09:46 +0800

    core

--- base/module/core/ModuleInfo.ts
+++ base/module/core/ModuleInfo.ts
@@ -1,12 +1,22 @@
 import {IModuleInfo} from "../interfaces/IModuleInfo";
+import {IModuleLoadList} from "../interfaces/IModuleLoadList";
 
 export class ModuleInfo implements IModuleInfo {
+    protected m_LoadList: IModuleLoadList;
     protected m_Name: string;
     protected m_Data: any;
 
     public constructor(name: string, data: any = null) {
         this.m_Name = name;
         this.m_Data = data;
+    }
+
+    public set loadList(value: IModuleLoadList) {
+        this.m_LoadList = value;
+    }
+
+    public get loadList(): IModuleLoadList {
+        return this.m_LoadList;
     }
 
     public get name(): string {
