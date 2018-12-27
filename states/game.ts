796e98e52fe7f820dd83b1fd6f726015fc3ba1b4
buxin <buxin@yahoo.cn>
2018-12-27 11:33:41 +0800

    core

--- states/game.ts
+++ states/game.ts
@@ -3,6 +3,7 @@ import {ModuleTypeEnum} from "../base/module/base/ModuleType";
 import {MessageType} from "../common/const/MessageType";
 import {UI} from "../Assets";
 import {INineSliceImageResouce} from "../base/module/interfaces/IModuleLoadList";
+import {GameConfig} from "../GameConfig";
 
 export default class Game extends Phaser.State {
 
@@ -20,11 +21,13 @@ export default class Game extends Phaser.State {
         Globals.LayerManager.init(this.game);
         Globals.LayoutManager.init(this.game);
 
-        let chatResource: INineSliceImageResouce[] = [{key: UI.DialogBg.getName() , url: UI.DialogBg.getPNG(), top: 7, left: 7, right: 7, bottom: 7},
+        if (!GameConfig.isEditor) {
+          let chatResource: INineSliceImageResouce[] = [{key: UI.DialogBg.getName() , url: UI.DialogBg.getPNG(), top: 7, left: 7, right: 7, bottom: 7},
             {key: UI.InputBg.getName() , url: UI.InputBg.getPNG(), top: 4, left: 2, right: 2, bottom: 4},
             {key: UI.Button1.getName() , url: UI.Button1.getPNG(), top: 8, left: 9, right: 9, bottom: 10}
-            ];
-        Globals.ModuleManager.openModule(ModuleTypeEnum.CHAT, null, {nineslice_images: chatResource});
+          ];
+          Globals.ModuleManager.openModule(ModuleTypeEnum.CHAT, null, {nineslice_images: chatResource});
+        }
 
         if (Globals.DataCenter.SceneData.initialize) {
             this.onHandleEnterScene();
