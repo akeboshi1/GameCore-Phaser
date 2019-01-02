1577fe1c7ff635abc8906c0bf3b498e78f9f0cca
pyr <pyr>
2019-01-02 17:22:42 +0800

    commit game over proto

--- common/service/GameService.ts
+++ common/service/GameService.ts
@@ -21,10 +21,16 @@ class Handler extends BasePacketHandler {
   constructor() {
     super();
     this.addHandlerFun(op_client.OPCODE._OP_VIRTUAL_WORLD_RES_CLIENT_SELECT_CHARACTER, this.handleSelectCharacter);
+      this.addHandlerFun(op_client.OPCODE._OP_VIRTUAL_WORLD_RES_CLIENT_GAME_OVER, this.handleGameOver);
   }
 
   private handleSelectCharacter(packet: PBpacket): void {
     let character: op_client.OP_VIRTUAL_WORLD_RES_CLIENT_SELECT_CHARACTER = packet.content;
     Globals.DataCenter.PlayerData.setCharacterId(character.selectCharacterId);
+  }
+
+  private handleGameOver(packet: PBpacket): void {
+    let GameOver: op_client.OP_VIRTUAL_WORLD_RES_CLIENT_GAME_OVER = packet.content;
+    console.log(`GameOver ${GameOver.msg}`);
   }
 }
