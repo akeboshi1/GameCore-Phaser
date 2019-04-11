28b13f9e1213f554823572de5d9037e51c78d530
buxin <buxin@yahoo.cn>
2019-04-11 17:44:08 +0800

    core

--- interface/ISocketConnection.ts
+++ interface/ISocketConnection.ts
@@ -2,7 +2,6 @@ import {PacketHandler, PBpacket} from "net-socket-packet";
 
 export interface ISocketConnection {
     send(packet: PBpacket);
-    destory();
     addPacketListener(handler: PacketHandler): void;
     rmPacketListener(handler: PacketHandler): void;
 }
