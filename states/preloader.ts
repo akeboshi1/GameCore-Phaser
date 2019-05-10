62d164d8ded10b10199409528ed502f9ec868b07
buxin <buxin@yahoo.cn>
2019-05-10 17:33:31 +0800

    c

--- states/preloader.ts
+++ states/preloader.ts
@@ -1,9 +1,10 @@
-import {Atlases, EDITOR, Font, Load} from "../Assets";
+import {EDITOR, Font, Load, UI} from "../Assets";
 import {GameConfig} from "../GameConfig";
 import {IAtlasResource, IImageResource} from "../interface/IPhaserLoadList";
 export default class Preloader extends Phaser.State {
-    private preloadBarSprite: Phaser.Sprite = null;
-    private preloadFrameSprite: Phaser.Sprite = null;
+    // private preloadBarSprite: Phaser.Sprite = null;
+    // private preloadFrameSprite: Phaser.Sprite = null;
+    private preloadingSprite: Phaser.Sprite = null;
 
     public preload(): void {
         if (GameConfig.preLoadList !== undefined) {
@@ -14,22 +15,6 @@ export default class Preloader extends Phaser.State {
             this.loadAtlases(GameConfig.preLoadList.atlas);
           }
         }
-        // todo: 测试
-
-        // let texturePath = "404/terrains/5caea3b530542d555307c2ac/1/5caea3b530542d555307c2ac.png";
-        // let dataPath = "404/terrains/5caea3b530542d555307c2ac/1/5caea3b530542d555307c2ac.json";
-        // let key: string = Load.Atlas.getKey(texturePath + dataPath);
-        // this.game.load.atlas(key, Load.Url.getRes(texturePath), Load.Url.getRes(dataPath));
-
-        // texturePath = "404/terrains/5caea2f330542d555307c2a5/1/5caea2f330542d555307c2a5.png";
-        // dataPath = "404/terrains/5caea2f330542d555307c2a5/1/5caea2f330542d555307c2a5.json";
-        // key = Load.Atlas.getKey(texturePath + dataPath);
-        // this.game.load.atlas(key, Load.Url.getRes(texturePath), Load.Url.getRes(dataPath));
-
-        // texturePath = "404/terrains/5caea39330542d555307c2a9/1/5caea39330542d555307c2a9.png";
-        // dataPath = "404/terrains/5caea39330542d555307c2a9/1/5caea39330542d555307c2a9.json";
-        // key = Load.Atlas.getKey(texturePath + dataPath);
-        // this.game.load.atlas(key, Load.Url.getRes(texturePath), Load.Url.getRes(dataPath));
 
         this.loadFonts();
         this.loadScripts();
@@ -41,19 +26,24 @@ export default class Preloader extends Phaser.State {
     public init(): void {
         // Setup your loading screen and preload sprite (if you want a loading progress indicator) here
 
-        this.preloadBarSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, Atlases.AtlasesPreloadSpritesArray.getName(), Atlases.AtlasesPreloadSpritesArray.Frames.PreloadBar);
-        this.preloadBarSprite.anchor.setTo(0, 0.5);
-        this.preloadBarSprite.x -= this.preloadBarSprite.width * 0.5;
+        // this.preloadBarSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, Atlases.AtlasesPreloadSpritesArray.getName(), Atlases.AtlasesPreloadSpritesArray.Frames.PreloadBar);
+        // this.preloadBarSprite.anchor.setTo(0, 0.5);
+        // this.preloadBarSprite.x -= this.preloadBarSprite.width * 0.5;
+        //
+        // this.preloadFrameSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, Atlases.AtlasesPreloadSpritesArray.getName(), Atlases.AtlasesPreloadSpritesArray.Frames.PreloadFrame);
+        // this.preloadFrameSprite.anchor.setTo(0.5);
 
-        this.preloadFrameSprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, Atlases.AtlasesPreloadSpritesArray.getName(), Atlases.AtlasesPreloadSpritesArray.Frames.PreloadFrame);
-        this.preloadFrameSprite.anchor.setTo(0.5);
+        // this.game.load.setPreloadSprite(this.preloadBarSprite);
 
-        this.game.load.setPreloadSprite(this.preloadBarSprite);
+        this.preloadingSprite = this.game.add.sprite(GameConfig.GameWidth - UI.Loading.getWidth() - 10 , GameConfig.GameHeight - UI.Loading.getHeight() - 10, UI.Loading.getName());
 
+        this.preloadingSprite.animations.add("load");
+        this.preloadingSprite.animations.play("load", 24, true);
     }
 
     public create(): void {
-        this.startGame();
+        this.game.camera.onFadeComplete.addOnce(this.startGame, this);
+        this.game.camera.fade(0x000000, 1000);
     }
 
     private loadImages(images: IImageResource[]): void {
@@ -83,11 +73,6 @@ export default class Preloader extends Phaser.State {
     }
 
     private startGame(): void {
-        this.game.camera.onFadeComplete.addOnce(this.loadTitle, this);
-        this.game.camera.fade(0x000000, 1000);
-    }
-
-    private loadTitle(): void {
         if (GameConfig.isEditor) {
             this.game.state.start("game");
         } else {
