1741f0b0a070852e0df2d7abca1145137f212654
guanxiaomin <guanxiaomin@qpowo.com>
2019-09-07 18:10:57 +0800

    joystick

--- src/scenes/main.ui.ts
+++ src/scenes/main.ui.ts
@@ -1,16 +1,50 @@
+import { Console } from "../utils/log";
+
 export class MainUIScene extends Phaser.Scene {
+  private bg: Phaser.GameObjects.Sprite;
+  private btn: Phaser.GameObjects.Sprite;
+  private joyStickCon: Phaser.GameObjects.Container;
+  private bgRadius: number;
+  private bgCenterPoint: Phaser.Geom.Point;
   private fps: Phaser.GameObjects.Text;
   constructor() {
     super({ key: MainUIScene.name });
   }
 
-  public preload() { }
+  public preload() {
+    this.load.atlas("joystick", "resources/button.png", "resources/button.json");
+  }
 
   public create() {
     this.fps = this.add.text(0, 0, "");
+    this.bg = this.add.sprite(0, 0, "joystick", "1.png");
+    this.bgRadius = this.bg.width >> 1;
+    this.bgCenterPoint = new Phaser.Geom.Point(this.bg.x + this.bgRadius, this.bg.y + this.bgRadius);
+    this.btn = this.add.sprite(this.bg.x, this.bg.y, "joystick", "2.png");
+    this.btn.setInteractive();
+    this.joyStickCon = this.add.container(500, 500);
+    this.joyStickCon.add(this.bg);
+    this.joyStickCon.add(this.btn);
+    this.input.setDraggable(this.btn);
+    this.btn.on("drag", this.dragUpdate, this);
+    this.btn.on("dragend", this.dragStop, this);
   }
 
   public update() {
     this.fps.setText(this.game.loop.actualFps.toFixed());
+  }
+
+  private dragUpdate(pointer, dragX, dragY) {
+    let d = Math.sqrt(dragX * dragX + dragY * dragY);
+    if (d > this.bgRadius) { d = this.bgRadius; }
+    const r = Math.atan2(dragY, dragX);
+    this.btn.x = Math.cos(r) * d + this.bg.x;
+    this.btn.y = Math.sin(r) * d + this.bg.y;
+  }
+
+  private dragStop(pointer) {
+    this.btn.x = this.bg.x;
+    this.btn.y = this.bg.y;
+    Console.log("dragEnd");
   }
 }
