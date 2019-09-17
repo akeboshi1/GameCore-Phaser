d4e562d505412f67e500225ae4eb00a7620b2557
Seven <askdaddy@gmail.com>
2019-09-17 12:40:50 +0800

    修改了game 初始化的位置。

--- src/scenes/loading.ts
+++ src/scenes/loading.ts
@@ -1,33 +1,34 @@
-import { LoadingView } from "../ui/loadview";
-import { IRoomService } from "../rooms/room";
+const LOGO_MARGIN = 25;
 
 export class LoadingScene extends Phaser.Scene {
-  private mCallBack: () => void;
-  private mRoom: IRoomService;
-  constructor() {
-    super({ key: LoadingScene.name });
-  }
-
-  public preload() {
-    this.load.image("stars", "resources/stars.png");
-  }
-
-  // public init(data: any) {
-  //   this.scene.wake();
-  //   if (data) {
-  //     this.mRoom = data.room;
-  //     this.mCallBack = data.callBack;
-  //   }
-  // }
-
-  public create() {
-    const loadingView: LoadingView = new LoadingView(this);
-    if (this.mCallBack) {
-      this.mCallBack();
+    constructor() {
+        super({key: LoadingScene.name});
     }
-  }
 
-  getKey(): string {
-    return (this.sys.config as Phaser.Types.Scenes.SettingsConfig).key;
-  }
+    public preload() {
+        this.load.spritesheet("rabbit00.png", "/resources/rabbit00.png", {frameWidth: 150, frameHeight: 150});
+    }
+
+    public create() {
+        const width = this.scale.gameSize.width;
+        const height = this.scale.gameSize.height;
+        const rect = this.add.graphics();
+        rect.fillStyle(0x616161);
+        rect.fillRect(0, 0, width, height);
+        this.anims.create({
+            key: "loading_rabbit00",
+            frames: this.anims.generateFrameNumbers("rabbit00.png", {start: 0, end: 59}),
+            frameRate: 33,
+            yoyo: true,
+            repeat: -1
+        });
+        const x: number = width - 150 - LOGO_MARGIN;
+        const y: number = height - 150 - LOGO_MARGIN;
+        const lo = this.add.sprite(x, y, "rabbit00.png");
+        lo.anims.play("loading_rabbit00");
+    }
+
+    getKey(): string {
+        return (this.sys.config as Phaser.Types.Scenes.SettingsConfig).key;
+    }
 }
