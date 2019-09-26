fa29ecbeb757d8ae220f4c63291a4ac0a9de6591
Seven <askdaddy@gmail.com>
2019-09-26 09:59:57 +0800

    改webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,27 @@ const phaser = path.join(pathToPhaser, 'dist/phaser.js');
 const ConfigWebpackPlugin = require("config-webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const TSLintPlugin = require("tslint-webpack-plugin");
 const appVer = require('./version');
 
-module.exports = {
+const config = {
     entry: {
         tooqing: path.join(__dirname, "./launcher.ts")
     },
     output: {
-        path: path.join(__dirname, 'dist/'),
+        path: path.resolve(__dirname, 'dist'),
         filename: "js/[name].js",
         chunkFilename: `js/[name]_v${appVer}.js`,
         libraryTarget: 'umd',
         globalObject: "this",
         library: "Tooqing"
     },
-    devtool: "source-map",
     module: {
         rules: [
-            { test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' },
-            { test: /phaser\.js$/, loader: 'expose-loader?Phaser' },
-            { test: /dragonBones\.js$/, loader: 'expose-loader?dragonBones' },
+            {test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/'},
+            {test: /phaser\.js$/, loader: 'expose-loader?Phaser'},
+            {test: /dragonBones\.js$/, loader: 'expose-loader?dragonBones'},
         ],
     },
     resolve: {
@@ -45,7 +44,7 @@ module.exports = {
         }),
         new ConfigWebpackPlugin(),
         new CopyWebpackPlugin([
-            { from: "**/*", to: "resources", force: true, context: 'resources' }
+            {from: "**/*", to: "resources", force: true, context: 'resources'}
         ]),
         new HtmlWebpackPlugin({
             inject: "head",
@@ -73,3 +72,6 @@ module.exports = {
         open: false
     }
 };
+module.exports = (env, argv) => {
+    return config;
+}
