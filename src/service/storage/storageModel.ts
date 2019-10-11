f3e1fbe86c5edd3c3803a71613139233ff5021fb
guanxiaomin <guanxiaomin@qpowo.com>
2019-10-11 11:42:17 +0800

    storage mvc

--- src/service/storage/storageModel.ts
+++ src/service/storage/storageModel.ts
@@ -1,9 +1,12 @@
 import { IBaseModel } from "../baseModel";
 import { PacketHandler } from "net-socket-packet";
+import { WorldService } from "../../game/world.service";
+import { ConnectionService } from "../../net/connection.service";
 
 export class StorageModel extends PacketHandler implements IBaseModel {
     public static NAME: string = "StorageModel";
     public initialize: boolean = false;
+    private mConnect: ConnectionService;
     constructor(private mWorld: WorldService) {
         super();
         this.mWorld.connection.addPacketListener(this);
---
+++ src/ui/bag/storage/storageMediator.ts
@@ -0,0 +1,37 @@
+import { IMediator } from "../../baseMediator";
+import { WorldService } from "../../../game/world.service";
+import { IAbstractPanel } from "../../abstractPanel";
+import { StoragePanel } from "./storagePanel";
+
+export class StorageMediator implements IMediator {
+    public world: WorldService;
+    private mView: StoragePanel;
+    constructor() {
+
+    }
+
+    public isSceneUI(): boolean {
+        return false;
+    }
+    public isShow(): boolean {
+        return this.mView.isShow();
+    }
+    public resize() {
+
+    }
+    public getView(): IAbstractPanel {
+        return this.mView;
+    }
+
+    public show(param?: any): void {
+        if (this.mView) this.mView.show(param);
+    }
+
+    public update(param?: any): void {
+        if (this.mView) this.mView.update(param);
+    }
+
+    public hide(): void {
+        if (this.mView) this.mView.hide();
+    }
+}
