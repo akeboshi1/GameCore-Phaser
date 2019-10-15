107e33e600417d7600fb14e103a36c6ca7374c89
guanxiaomin <guanxiaomin@qpowo.com>
2019-10-15 15:43:08 +0800

    add Shop code

---
+++ src/ui/Shop/ShopMediator.ts
@@ -0,0 +1,151 @@
+import { IMediator } from "../baseMediator";
+import { WorldService } from "../../game/world.service";
+import { IAbstractPanel } from "../abstractPanel";
+import { ShopModel } from "../../service/shop/shopModel";
+import { MessageType } from "../../const/MessageType";
+import { PBpacket } from "net-socket-packet";
+import { op_virtual_world, op_def, op_client, op_gameconfig } from "pixelpai_proto";
+import { ShopPanel } from "./ShopPanel";
+import { ILayerManager } from "../layer.manager";
+import { BagModel } from "../../service/bag/bagModel";
+
+export class ShopMediator implements IMediator {
+    public static NAME: string = "ShopMediator";
+    public world: WorldService;
+    private mView: ShopPanel;
+    private mShopModel: ShopModel;
+    private readonly _perPage = 50;
+    private _curPage: number;
+    private fetching: boolean;
+    private isEnd = false;
+    private mParam: any;
+    private mScene: Phaser.Scene;
+    constructor(layerManager: ILayerManager, scene: Phaser.Scene, world: WorldService) {
+        this.world = world;
+        this.mScene = scene;
+        this.mShopModel = this.world.modelManager.getModel(ShopModel.NAME) as ShopModel;
+    }
+
+    public isSceneUI(): boolean {
+        return false;
+    }
+
+    public resize() {
+        if (this.mView) return this.mView.resize();
+    }
+
+    public getView(): IAbstractPanel {
+        return this.mView;
+    }
+
+    public show(param?: any) {
+        if (this.mView && this.mView.isShow()) {
+            return;
+        }
+        this.mView = new ShopPanel(this.mScene, this.world);
+        this.mView.show(param);
+        // this.world.uiManager.getUILayerManager().addToUILayer(this.mView);
+        this.mParam = param;
+        this.mShopModel.register();
+        this.world.modelManager.on(MessageType.QUERY_PACKAGE, this.queryPackageHandler, this);
+        this.world.modelManager.on(MessageType.SYNC_USER_BALANCE, this.onSyncUserBalanceHandler, this);
+        (this.world.modelManager.getModel(BagModel.NAME) as BagModel).requestVirtualWorldQueryPackage(param[0].id, 1, ShopPanel.ShopSlotCount);
+    }
+
+    public update(param?: any) {
+        if (this.mView) this.mView.update(param);
+    }
+
+    public hide() {
+        if (this.mView) {
+            this.mView.hide();
+        }
+        this.mShopModel.unRegister();
+        this.world.modelManager.off(MessageType.QUERY_PACKAGE, this.queryPackageHandler, this);
+        this.world.modelManager.off(MessageType.SYNC_USER_BALANCE, this.onSyncUserBalanceHandler, this);
+    }
+
+    public destroy() {
+        if (this.mShopModel) {
+            this.mShopModel.destroy();
+            this.mShopModel = null;
+        }
+        this.world = null;
+        this.mScene = null;
+        this._curPage = 0;
+        this.fetching = false;
+        this.isEnd = false;
+        this.mParam = null;
+        if (this.mView) {
+            this.mView.destroy();
+            this.mView = null;
+        }
+    }
+
+    public isShow(): boolean {
+        return this.mView ? this.mView.isShow() : false;
+    }
+
+    private queryShopProps(page: number = 1, perPage: number = 50) {
+        if (!!this.mParam === false) return;
+        this.fetching = true;
+
+        const pkt: PBpacket = new PBpacket(op_virtual_world.OPCODE._OP_CLIENT_REQ_VIRTUAL_WORLD_QUERY_PACKAGE);
+        const content: op_virtual_world.IOP_CLIENT_REQ_VIRTUAL_WORLD_QUERY_PACKAGE = pkt.content;
+        content.id = this.mParam[0].id;
+        content.page = page;
+        content.perPage = perPage;
+        this.world.connection.send(pkt);
+    }
+
+    private queryPackageHandler(data: op_client.IOP_VIRTUAL_WORLD_RES_CLIENT_QUERY_PACKAGE) {
+        if (data.items.length > 0 && data.id === this.mParam[0].id) {
+            this.mView.setDataList(data.items);
+            //  this.mView.addItems(data.items);
+        } else {
+            this.isEnd = true;
+        }
+        this.fetching = false;
+    }
+
+    private onBuyItemHandler(item: op_gameconfig.IItem) {
+        const prices = item.price;
+        for (const price of prices) {
+            if (price.coinType === op_def.CoinType.TU_DING_COIN) {
+                // if (price.price > this.mView.price) {
+                // todo alert
+                // Globals.PromptManager.showAlert("图钉不足，是否前往充值？", this.onPayTuDingHandler, this, AlertmView.OK | AlertmView.CANCEL);
+                // return;
+                // }
+            }
+        }
+        const pkt: PBpacket = new PBpacket(op_virtual_world.OPCODE._OP_CLIENT_REQ_VIRTUAL_WORLD_TARGET_UI);
+        const content: op_virtual_world.OP_CLIENT_REQ_VIRTUAL_WORLD_TARGET_UI = pkt.content;
+        content.uiId = this.mParam[0].id;
+        content.componentId = item.id;
+        this.world.connection.send(pkt);
+        // Globals.SocketManager.send(pkt);
+    }
+
+    private syncUserBalance() {
+        const pkt: PBpacket = new PBpacket(op_virtual_world.OPCODE._OP_CLIENT_REQ_VIRTUAL_WORLD_SYNC_USER_BALANCE);
+        this.world.connection.send(pkt);
+        // Globals.SocketManager.send(pkt);
+    }
+
+    private onChangeIndex(rate) {
+        if (rate >= 0.9 && this.fetching === false && this.isEnd === false) {
+            this._curPage++;
+            this.queryShopProps(this._curPage);
+        }
+    }
+
+    private onSyncUserBalanceHandler(balance: op_client.IOP_VIRTUAL_WORLD_RES_CLIENT_SYNC_USER_BALANCE) {
+        if (!!this.mView === false) { return; }
+        // this.mView.setUserBalance(balance);
+    }
+
+    private onPayTuDingHandler() {
+        // shell.openExternal("https://www.tooqing.com/recharge");
+    }
+}
---
+++ src/ui/Shop/ShopPanel.ts
@@ -0,0 +1,145 @@
+import { Panel } from "../components/panel";
+import { WorldService } from "../../game/world.service";
+import { Url } from "../../utils/resUtil";
+import { ItemSlot } from "../bag/item.slot";
+import { ShopItemSlot } from "./shop.itemSlot";
+import { Logger } from "../../utils/log";
+import { Size } from "../../utils/size";
+
+export class ShopPanel extends Panel {
+    public static ShopSlotCount: number = 20;
+    private mWorld: WorldService;
+    private mShopItemSlotList: ShopItemSlot[];
+    private mClsBtnSprite: Phaser.GameObjects.Sprite;
+    private mDataList: any[];
+    constructor(scene: Phaser.Scene, world: WorldService) {
+        super(scene);
+        this.mWorld = world;
+    }
+
+    public resize() {
+        const size: Size = this.mWorld.getSize();
+        this.x = size.width >> 1;
+        this.y = size.height - 300;
+    }
+    public hide() {
+        super.hide();
+        this.destroy();
+    }
+
+    public setDataList(value: any[]) {
+        this.mDataList = value;
+        if (!this.mInitialized) {
+            return;
+        }
+        this.refreshDataList();
+    }
+
+    public destroy() {
+        if (this.mShopItemSlotList) {
+            this.mShopItemSlotList.forEach((slot: ShopItemSlot) => {
+                if (slot) slot.destroy();
+            });
+            this.mShopItemSlotList.length = 0;
+            this.mShopItemSlotList = null;
+        }
+        if (this.mDataList) {
+            this.mDataList.length = 0;
+            this.mDataList = null;
+        }
+        this.mInitialized = false;
+        super.destroy();
+    }
+
+    protected preload() {
+        if (!this.mScene) {
+            return;
+        }
+        this.mScene.load.atlas("shopView", Url.getRes("ui/shop/shopView.png"), Url.getRes("ui/shop/shopView.json"));
+        super.preload();
+    }
+
+    protected init() {
+        if (this.mInitialized) return;
+        super.init();
+        const size: Size = this.mWorld.getSize();
+        this.x = size.width >> 1;
+        this.y = size.height - 300;
+        const mBg: Phaser.GameObjects.Sprite = this.mScene.make.sprite(undefined, false);
+        mBg.setTexture("shopView", "shopView_bg");
+        mBg.x = 0;
+        mBg.y = 0;
+        this.setSize(mBg.width, mBg.height);
+        this.addAt(mBg, 0);
+
+        const titleCon: Phaser.GameObjects.Sprite = this.mScene.make.sprite(undefined, false);
+        titleCon.setTexture("shopView", "shopView_titleIcon");
+        titleCon.x = (-mBg.width >> 1) + 35;
+        titleCon.y = -mBg.height >> 1;
+        this.add(titleCon);
+        const titleTF: Phaser.GameObjects.Text = this.mScene.make.text(undefined, false);
+
+        titleTF.setFontFamily("Tahoma");
+        titleTF.setFontStyle("bold");
+        titleTF.setFontSize(20);
+        titleTF.setText("内购商城");
+        titleTF.x = titleCon.x + titleCon.width - 10;
+        titleTF.y = titleCon.y - (titleTF.height >> 1);
+        this.add(titleTF);
+        this.mShopItemSlotList = [];
+        let itemSlot: ShopItemSlot;
+        let tmpX: number;
+        let tmpY: number;
+        for (let i: number = 0; i < 20; i++) {
+            tmpX = (i % 5) * 100 - mBg.width / 2 + 75;
+            tmpY = Math.floor(i / 5) * 90 - mBg.height / 2 + 80;
+            itemSlot = new ShopItemSlot(this.mScene, this.mWorld, this, tmpX, tmpY, "shopView", "ui/shop/shopView.png", "ui/shop/shopView.json", "shopView_bagSlot", null, null);
+            itemSlot.createUI();
+            this.mShopItemSlotList.push(itemSlot);
+        }
+        if (!this.mScene.cache.obj.has("clsBtn")) {
+            this.mScene.load.spritesheet("clsBtn", "resources/ui/common/common_clsBtn.png", { frameWidth: 16, frameHeight: 16, startFrame: 1, endFrame: 3 });
+            this.mScene.load.once(Phaser.Loader.Events.COMPLETE, this.onClsLoadCompleteHandler, this);
+            this.mScene.load.start();
+        } else {
+            this.onClsLoadCompleteHandler();
+        }
+        this.mWorld.uiManager.getUILayerManager().addToToolTipsLayer(this);
+
+        // 异步加载过程中会导致数据过来，面板仍然没有加载完毕，所以缓存数据等ui加载完毕再做显示
+        if (this.mDataList) {
+            this.refreshDataList();
+        }
+    }
+
+    protected loadComplete(loader: Phaser.Loader.LoaderPlugin, totalComplete: integer, totalFailed: integer) {
+        if (this.mInitialized) {
+            return;
+        }
+        this.init();
+    }
+
+    private onClsLoadCompleteHandler() {
+        this.mClsBtnSprite = this.mScene.make.sprite(undefined, false);
+        this.mClsBtnSprite.setTexture("clsBtn", "btn_normal");
+        this.mClsBtnSprite.x = (this.width >> 1) - 65;
+        this.mClsBtnSprite.y = (- this.height >> 1);
+        this.mClsBtnSprite.setInteractive();
+        this.mClsBtnSprite.on("pointerup", this.hide, this);
+        this.add(this.mClsBtnSprite);
+    }
+
+    private refreshDataList() {
+        if (!this.mDataList) {
+            Logger.error("this.mDataList is undefiend");
+            return;
+        }
+        const len = ShopPanel.ShopSlotCount;
+        let item: ItemSlot;
+        for (let i = 0; i < len; i++) {
+            item = this.mShopItemSlotList[i];
+            if (!item) continue;
+            item.dataChange(this.mDataList[i]);
+        }
+    }
+}
