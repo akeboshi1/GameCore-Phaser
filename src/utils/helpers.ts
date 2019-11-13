2f642ad6406ec023fb196fa5720dd95966a3035e
ZhangTeng <zhangteng@outlook.com>
2019-11-13 14:05:15 +0800

    id生成规则

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,10 +1,11 @@
 import * as Chance from "chance";
 
 export default class Helpers {
+    static readonly MAX_ID: number = Math.pow(2, 31);
     static genId(): number {
         return new Chance().natural({
             min: 10000,
-            max: Number.MAX_VALUE
+            max: Helpers.MAX_ID
         });
     }
 }
