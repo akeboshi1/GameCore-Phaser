a9ac82ff8ab21c3b32792fbe1673a1b05c16c355
ZhangTeng <zhangteng@outlook.com>
2019-11-22 15:10:53 +0800

    修复rows cols不一致的情况下地图地块位置问题

--- src/utils/position45.ts
+++ src/utils/position45.ts
@@ -13,7 +13,7 @@ export interface IPosition45Obj {
 
 export class Position45 {
   public static transformTo90(point: Pos, position: IPosition45Obj): Pos {
-    const offsetX = position.sceneWidth / 2; // + position.tileWidth / 2;
+    const offsetX = position.rows * (position.tileWidth / 2); // + position.tileWidth / 2;
     return new Pos(
       (point.x - point.y) * (position.tileWidth / 2) + offsetX,
       ((point.x + point.y)) * (position.tileHeight / 2)
@@ -21,7 +21,8 @@ export class Position45 {
   }
 
   public static transformTo45(point3: Pos, position: IPosition45Obj): Pos {
-    const offsetX = position.sceneWidth / 2; // - position.tileWidth / 2;
+    const offsetX = position.rows * (position.tileWidth / 2);
+    // const offsetX = position.sceneWidth / 2; // - position.tileWidth / 2;
     return new Pos(
       Math.floor((point3.y + point3.z) / position.tileHeight + (point3.x - offsetX) / position.tileWidth),
       Math.floor((point3.y + point3.z) / position.tileHeight - (point3.x - offsetX) / position.tileWidth),
