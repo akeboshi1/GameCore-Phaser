a102f6b4ca1db6d574a959beb92e8bc3d9cf60f4
ZhangTeng <zhangteng@outlook.com>
2020-03-27 10:10:29 +0800

    fixed bug

---
+++ src/rooms/display/wall.display.ts
@@ -0,0 +1,78 @@
+import { DisplayObject } from "./display.object";
+import { IRoomService } from "../room";
+import { ElementDisplay } from "./element.display";
+import { IFramesModel } from "./frames.model";
+import { IDragonbonesModel } from "./dragonbones.model";
+import { AnimationData } from "../element/sprite";
+import { op_def } from "pixelpai_proto";
+import { IElement } from "../element/element";
+
+export class WallDisplay extends Phaser.GameObjects.Container implements ElementDisplay {
+  protected readonly roomService: IRoomService;
+  private mImage: Phaser.GameObjects.Image;
+  constructor(scene: Phaser.Scene, roomService: IRoomService) {
+    super(scene, 0, 0, undefined);
+    this.roomService = roomService;
+  }
+
+  loadDisplay(texture: string, data: string) {
+    // const key = texture + data;
+    const key = "wall";
+    if (this.scene.textures.exists(key)) {
+      this.onLoadCompleteHandler();
+    } else {
+      this.scene.load.once(Phaser.Loader.Events.COMPLETE, this.onLoadCompleteHandler, this);
+      this.scene.load.atlas(key, texture, data);
+      this.scene.load.start();
+    }
+  }
+
+  load(data: IFramesModel | IDragonbonesModel) {
+  }
+
+  changeAlpha(val?: number) {
+  }
+
+  play(animationName: AnimationData) { }
+
+  removeFromParent(): void { }
+
+  fadeIn(callback?: () => void) { }
+
+  fadeOut(callback?: () => void) { }
+
+  showNickname(val: string) { }
+
+  setDisplayBadges(cards: op_def.IBadgeCard[]) { }
+
+  showRefernceArea() { }
+
+  hideRefernceArea() { }
+
+  showEffect() { }
+
+  private onLoadCompleteHandler() {
+    this.mImage = this.scene.make.image({
+      key: "wall",
+      frame: "wall_up.png"
+    }, false);
+    this.add(this.mImage);
+    this.emit("initialized", this);
+  }
+
+  get sortX(): number {
+    return 0;
+  }
+
+  get sortY(): number {
+    return 0;
+  }
+
+  get baseLoc(): Phaser.Geom.Point {
+    return;
+  }
+
+  get element(): IElement {
+    return;
+  }
+}
--- src/rooms/wall/wall.manager.ts
+++ src/rooms/wall/wall.manager.ts
@@ -2,20 +2,26 @@ import { IRoomService } from "../room";
 import { Wall, Direction } from "./wall";
 import { PacketHandler, PBpacket } from "net-socket-packet";
 import { op_client } from "pixelpai_proto";
-
+import { Pos } from "../../utils/pos";
 export class WallManager extends PacketHandler  {
   private mWalls: Map<number, Wall> = new Map<number, Wall>();
   constructor(protected mRoom: IRoomService) {
     super();
 
-    this.addHandlerFun(op_client.OPCODE._OP_VIRTUAL_WORLD_REQ_CLIENT_ADD_SPRITE_END, this.onDawWallHandler);
+    if (this.mRoom.connection) {
+      this.mRoom.connection.addPacketListener(this);
+      this.addHandlerFun(op_client.OPCODE._OP_VIRTUAL_WORLD_REQ_CLIENT_ADD_SPRITE_END, this.onDawWallHandler);
+    }
   }
 
   protected _add(x: number, y: number, dir: Direction) {
-    const wall = new Wall();
+    // const wall = new Wall(this.mRoom);
+    // wall.setPosition(x, y);
   }
 
   private onDawWallHandler(packet: PBpacket) {
     const terrain = this.mRoom.terrainManager;
+    const pos = this.mRoom.transformTo90(new Pos(0, 0));
+    this._add(pos.x, pos.y, Direction.UP);
   }
 }
