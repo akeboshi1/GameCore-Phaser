b3c702eece87a6ed3f137cd7ca0f4c669b2852ef
404 <zhangteng@outlook.com>
2020-04-12 17:44:47 +0800

    minecar

--- src/ui/MineCar/MineCarPanel.ts
+++ src/ui/MineCar/MineCarPanel.ts
@@ -3,6 +3,10 @@ import { Font } from "../../utils/font";
 import { op_def } from "pixelpai_proto";
 import { DynamicImage } from "../components/dynamic.image";
 import { BasePanel } from "../components/BasePanel";
+import GridTable from "../../../lib/rexui/lib/ui/gridtable/GridTable";
+import { Button } from "../components/button";
+import { Logger } from "../../utils/log";
+import { CheckboxGroup } from "../components/checkbox.group";
 
 export class MineCarPanel extends BasePanel {
   private readonly key = "mine_car";
@@ -10,6 +14,11 @@ export class MineCarPanel extends BasePanel {
   private mMask: Phaser.GameObjects.Graphics;
   private mCloseBtn: Phaser.GameObjects.Image;
   private mCounter: Phaser.GameObjects.Text;
+  private mPropGrid: GridTable;
+  private mPropContainer: Phaser.GameObjects.Container;
+  private mCategorieContainer: Phaser.GameObjects.Container;
+  private mHelpBtn: Phaser.GameObjects.Image;
+  private mTips: Tips;
   constructor(scene: Phaser.Scene, world: WorldService) {
     super(scene, world);
     this.setTween(false);
@@ -26,6 +35,12 @@ export class MineCarPanel extends BasePanel {
     this.mMask.setInteractive(new Phaser.Geom.Rectangle(-width / 2, -height / 2, width, height), Phaser.Geom.Rectangle.Contains);
     // this.mPanel.y = -
 
+    this.mPropGrid.x = this.x;
+    this.mPropGrid.y = this.y + 16 * this.dpr;
+    this.mPropGrid.layout();
+    this.mPropContainer.x = -this.x;
+    this.mPropContainer.y = -this.y; // + 8 * this.dpr;
+
     this.setSize(width, height);
     // this.setInteractive(new Phaser.Geom.Rectangle(0, 0, width, height), Phaser.Geom.Rectangle.Contains);
   }
@@ -37,6 +52,21 @@ export class MineCarPanel extends BasePanel {
       {key: "宝石", value: "宝石"},
       {key: "化石", value: "化石"},
       {key: "杂物", value: "杂物"}];
+    const items = [];
+    const frame = this.scene.textures.getFrame(this.key, "nav_btn_normal.png").width;
+    const gap = (this.mCategorieContainer.width - frame / 2 - subcategorys.length * frame) / ((subcategorys.length - 1));
+    for (let i = 0; i < subcategorys.length; i++) {
+      const item = new Button(this.scene, this.key, "nav_btn_normal.png", "nav_btn_down.png", subcategorys[i].value);
+      item.x = i * (item.width + gap * this.dpr) + item.width / 2;
+      item.y = (this.mCategorieContainer.height - item.height) / 2;
+      item.setTextColor("#566ddb");
+      items.push(item);
+    }
+
+    const checkbox = new CheckboxGroup();
+    checkbox.appendItemAll(items);
+    checkbox.on("selected", this.onClickCategoryHandler, this);
+    this.mCategorieContainer.add(items);
   }
 
   register() {
@@ -45,6 +75,17 @@ export class MineCarPanel extends BasePanel {
     }
     this.mCloseBtn.setInteractive();
     this.mCloseBtn.on("pointerup", this.onCloseHandler, this);
+  }
+
+  setProp() {
+
+  }
+
+  destroy() {
+    if (this.mPropGrid) {
+      this.mPropGrid.destroy();
+    }
+    super.destroy();
   }
 
   protected preload() {
@@ -70,7 +111,7 @@ export class MineCarPanel extends BasePanel {
     }, false);
 
     this.mCounter = this.scene.make.text({
-      x: -84 * this.dpr,
+      x: -86 * this.dpr,
       y: bg.height / 2 - 23 * this.dpr,
       text: "25/50",
       style: {
@@ -80,22 +121,86 @@ export class MineCarPanel extends BasePanel {
       }
     }, false);
 
+    this.mTips = new Tips(this.scene, this.key, this.dpr, this.scale);
+    this.mTips.x = 40 * this.dpr;// - this.mTips.width / 2;
+    this.mTips.y = -130 * this.dpr - this.mTips.height / 2;
+
+    this.mCategorieContainer = this.scene.make.container(undefined, false);
     const categoriesBg = this.scene.make.image({
       key: this.key,
       frame: "nav_bg.png"
     });
     categoriesBg.y = -111 * this.dpr + categoriesBg.height / 2;
+    this.mCategorieContainer.setSize(categoriesBg.width * this.scale, categoriesBg.height * this.scale);
+    this.mCategorieContainer.x = -categoriesBg.width / 2;
+    this.mCategorieContainer.y = categoriesBg.y;
+
+    this.mPropContainer = this.scene.make.container(undefined, false);
+    const propFrame = this.scene.textures.getFrame(this.key, "item_boder.png");
+    const capW = (propFrame.width + 4 * this.dpr);
+    const capH = (propFrame.height + 4 * this.dpr);
+    const w = this.scene.cameras.main.width;
+    const zoom = this.scale;
+    this.mPropGrid = new GridTable(this.scene, {
+      x: 0,
+      y: 16 * this.dpr,
+      table: {
+        width: 190 * this.dpr * zoom,
+        height: 190 * this.dpr * zoom,
+        columns: 5,
+        cellWidth: capW,
+        cellHeight: capH,
+        reuseCellContainer: true,
+      },
+      clamplChildOY: true,
+      createCellContainerCallback: (cell, cellContainer) => {
+        const scene = cell.scene,
+          item = cell.item;
+        if (cellContainer === null) {
+          cellContainer = new Item(scene, this.key, this.dpr);
+          this.mPropContainer.add(cellContainer);
+        }
+        cellContainer.setData({ item });
+        cellContainer.setProp(item);
+        return cellContainer;
+      },
+    });
+    this.mPropGrid.layout();
+    this.mPropGrid.on("cell.1tap", (cell) => {
+      const item = cell.getData("item");
+      if (item) {
+        this.onSelectItemHandler(item);
+      }
+    });
+    this.mHelpBtn = this.scene.make.image({
+      x: this.mCounter.x + this.mCounter.width + 14 * this.dpr,
+      y: this.mCounter.y,
+      key: this.key,
+      frame: "help.png"
+    }, false).setOrigin(0);
 
     this.add(this.mPanel);
-    this.mPanel.add([this.mMask, bg, this.mCloseBtn, this.mCounter, categoriesBg]);
+    this.mPanel.add([this.mMask, bg, this.mCloseBtn, this.mTips, this.mCounter, categoriesBg, this.mCategorieContainer, this.mPropContainer, this.mHelpBtn]);
     super.init();
     this.resize(this.scene.cameras.main.width, this.scene.cameras.main.height);
+    const items = new Array(50).fill({item: "", locked: true});
+    this.mPropGrid.setItems(items);
+
+    this.setCategories([]);
 
     this.register();
   }
 
   private onCloseHandler() {
     this.emit("close");
+  }
+
+  private onSelectItemHandler(item: Item) {
+
+  }
+
+  private onClickCategoryHandler() {
+    Logger.getInstance().log("clicked");
   }
 }
 
@@ -108,13 +213,58 @@ class Item extends Phaser.GameObjects.Container {
 
     const border = this.scene.make.image({
       key,
-      frame: "border.png"
-    }, false);
+      frame: "item_boder.png"
+    }, false).setOrigin(0);
+    this.setSize(border.width, border.height);
 
     this.mItem = new DynamicImage(this.scene, 0, 0);
-    this.add([border, this.mItem, this.mCounter]);
+
+    this.mLockImg = this.scene.make.image({
+      x: border.width / 2,
+      y: border.height / 2,
+      key,
+      frame: "lock.png"
+    }, false);
+    this.add([border, this.mItem]);
   }
 
-  setItem(data: any) {
+  setProp(data: any) {
+    if (data.locked) {
+      this.add(this.mLockImg);
+    } else {
+      this.remove(this.mLockImg);
+    }
+  }
+}
+
+class Tips extends Phaser.GameObjects.Container {
+  private mName: Phaser.GameObjects.Text;
+  private mDes: Phaser.GameObjects.Text;
+  constructor(scene: Phaser.Scene, key: string, dpr: number, zoom: number = 1) {
+    super(scene);
+    const bg = this.scene.make.image({
+      key,
+      frame: "tips_bg.png"
+    }, false);
+    this.mName = this.scene.make.text({
+      style: {
+        fontFamily: Font.DEFULT_FONT,
+        fontSize: 14 * dpr,
+        colof: "#2d3a56"
+      }
+    }, false);
+    this.mDes = this.scene.make.text({
+      style: {
+        fontFamily: Font.DEFULT_FONT,
+        fontSize: 14 * dpr,
+        color: "#4e4b4b"
+      }
+    }, false);
+    this.add([bg, this.mName, this.mDes]);
+    this.setSize(bg.width * zoom, bg.height * zoom);
+  }
+
+  setItem() {
+
   }
 }
