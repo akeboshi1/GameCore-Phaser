711ce92647282263b6f4c4595a3a5059993af4c7
zhangteng <zhangteng@apowo.com>
2020-06-05 09:40:58 +0800

    hide usermenu

--- src/ui/PicaMessageBox/PicaMessageBoxPanel.ts
+++ src/ui/PicaMessageBox/PicaMessageBoxPanel.ts
@@ -13,7 +13,7 @@ export class PicaMessageBoxPanel extends BasePanel {
   private mButtonContaier: Phaser.GameObjects.Container;
   constructor(scene: Phaser.Scene, world: WorldService) {
     super(scene, world);
-    this.disInteractive();
+    // this.disInteractive();
   }
 
   show(param) {
@@ -78,6 +78,14 @@ export class PicaMessageBoxPanel extends BasePanel {
   }
 
   protected init() {
+    const { width, height } = this.scene.cameras.main;
+
+    const border = this.scene.make.graphics(undefined, false);
+    border.fillStyle(0, 0.6);
+    border.fillRect(-width * 0.5, -height * 0.5, width, height);
+    border.setInteractive(new Phaser.Geom.Rectangle(0, 0, width, height), Phaser.Geom.Rectangle.Contains);
+    this.add(border);
+
     const frame = this.scene.textures.getFrame(this.key, "bg");
     const background = new NinePatch(this.scene, 0, 0, 286 * this.dpr, frame.height, this.key, "bg", {
       left: 22 * this.dpr,
