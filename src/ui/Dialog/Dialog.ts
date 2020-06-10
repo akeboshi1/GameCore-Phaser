714a66011e53601d874ee7fffe8a4958aa10f9b6
liumeng <liumeng@apowo.com>
2020-06-10 14:51:53 +0800

    对话框

--- src/ui/Dialog/Dialog.ts
+++ src/ui/Dialog/Dialog.ts
@@ -3,7 +3,7 @@ import { WorldService } from "../../game/world.service";
 import { op_def, op_client, op_virtual_world } from "pixelpai_proto";
 import { ConnectionService } from "../../net/connection.service";
 
-export class NpcDialogue extends PacketHandler {
+export class Dialog extends PacketHandler {
     private readonly world: WorldService;
     private mEvent: Phaser.Events.EventEmitter;
     constructor(world: WorldService) {
@@ -36,6 +36,14 @@ export class NpcDialogue extends PacketHandler {
     destroy() {
         this.unregister();
         this.mEvent.destroy();
+    }
+    public queryNextDialog(uiid: number, comid: number, data: number[]) {
+        const packet = new PBpacket(op_virtual_world.OPCODE._OP_CLIENT_REQ_VIRTUAL_WORLD_TARGET_UI);
+        const content: op_virtual_world.OP_CLIENT_REQ_VIRTUAL_WORLD_TARGET_UI = packet.content;
+        content.uiId = uiid;
+        content.componentId = comid;
+        content.data = data;
+        this.connection.send(packet);
     }
 
     get connection(): ConnectionService {
