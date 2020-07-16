bd46865a56815b17c3e52a62de896a2d1498f5a3
guanxiaomin <guanxiaomin@apowo.com>
2020-07-16 15:37:03 +0800

    game-core mod event update

--- src/game/world.events.ts
+++ src/game/world.events.ts
@@ -2,12 +2,22 @@ export enum SceneEvent {
     SCENE_CREATE = "scene_create",
     SCENE_CHANGE = "scene_change",
     SCENE_DESTROY = "scene_destroy",
-    SCENE_PAUSE="scene_pause",
-    SCENE_RESUME="scene_resume",
+    SCENE_PAUSE = "scene_pause",
+    SCENE_RESUME = "scene_resume",
 }
 
 export enum GameEvent {
     GAME_CREATE = "game_create",
     GAME_CLEAR = "game_clear",
     GAME_DESTROY = "game_destroy",
+}
+
+export enum UIEvent {
+    SHOW_UI = "show_ui",
+    HIDE_UI = "hide_ui"
+}
+
+export enum ModuleEvent {
+    MODULE_INIT = "module_init",
+    MODULE_DESTROY = "module_destroy",
 }
