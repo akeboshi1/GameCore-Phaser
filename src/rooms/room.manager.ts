bb329e0b5745906d7af5f69374da080982b792eb
zhangteng <zhangteng@apowo.com>
2020-07-29 19:13:36 +0800

    修复重复进入房间，侦听被移除

--- src/rooms/room.manager.ts
+++ src/rooms/room.manager.ts
@@ -113,29 +113,26 @@ export class RoomManager extends PacketHandler implements IRoomManager {
     private async onEnterScene(scene: op_client.IOP_VIRTUAL_WORLD_RES_CLIENT_ENTER_SCENE) {
         // this.destroy();
         const vw = scene;
-        let room: Room;
         if (this.hasRoom(vw.scene.id)) {
-            room = <Room>this.getRoom(vw.scene.id);
-            if (this.mCurRoom) {
-                await this.leaveScene(this.mCurRoom);
-            }
-            room.addActor(vw.actor);
-            room.enter(vw.scene);
-            this.mCurRoom = room;
+            this.onEnterRoom(scene);
         } else {
             // load this scene config in gameConfig
             this.world.loadSceneConfig(vw.scene.id.toString()).then(async (config: Lite) => {
                 this.world.elementStorage.setSceneConfig(config);
-                if (this.mCurRoom) {
-                    await this.leaveScene(this.mCurRoom);
-                }
-                room = new Room(this);
-                this.mRooms.push(room);
-                room.addActor(vw.actor);
-                room.enter(vw.scene);
-                this.mCurRoom = room;
+                this.onEnterRoom(scene);
             });
         }
+    }
+
+    private async onEnterRoom(scene: op_client.IOP_VIRTUAL_WORLD_RES_CLIENT_ENTER_SCENE) {
+        if (this.mCurRoom) {
+            await this.leaveScene(this.mCurRoom);
+        }
+        const room = new Room(this);
+        this.mRooms.push(room);
+        room.addActor(scene.actor);
+        room.enter(scene.scene);
+        this.mCurRoom = room;
     }
 
     private async onEnterDecorate(scene: op_client.IOP_VIRTUAL_WORLD_RES_CLIENT_ENTER_SCENE) {
