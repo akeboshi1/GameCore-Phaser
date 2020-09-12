b95f752a626a7024b782e33a9a999e9af0b6de38
zhangteng <zhangteng@apowo.com>
2020-09-12 17:33:16 +0800

    editor.launcher phaser changed to tooqingphaser

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -1,4 +1,4 @@
-import "phaser";
+import "tooqinggamephaser";
 export { EditorLauncher, EditorCanvasType } from "./editor.launcher";
 export { ElementEditorCanvas, ElementEditorEmitType, ElementEditorBrushType } from "./canvas/element/element.editor.canvas";
 export { AvatarEditorCanvas, AvatarEditorEmitType } from "./canvas/avatar/avatar.editor.canvas";
