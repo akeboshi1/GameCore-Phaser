bfe1603cbc64ff716023d7474eb945fd3a38b7de
guanxiaomin <guanxiaomin@apowo.com>
2020-10-14 18:02:20 +0800

    delete old code

--- rpc/old/ui/Bag/Idropable.ts
+++
@@ -1,9 +0,0 @@
-import { IDragable } from "./Idragable";
-
-export interface IDropable {
-    dragDrop(dragable: IDragable): void;
-    dragOver(dragable: IDragable): void;
-    getDropType(): number;
-    getDropData(): any;
-    getBound(): Phaser.Geom.Rectangle;
-}
--- rpc/old/ui/Chat/Message.ts
+++
@@ -1,6 +0,0 @@
-import { op_def } from "pixelpai_proto";
-export interface IMessage {
-    chat: string;
-    channel: op_def.ChatChannel;
-    color?: string;
-}
