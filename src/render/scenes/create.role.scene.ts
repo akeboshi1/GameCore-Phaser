70b30c30142ca0bf46d3e3d25a00631bc31a5ac8
guanxiaomin <guanxiaomin@apowo.com>
2020-11-17 11:31:22 +0800

    update createrolescenename

--- src/render/scenes/create.role.scene.ts
+++ src/render/scenes/create.role.scene.ts
@@ -1,12 +1,12 @@
 import { BasicScene } from "./basic.scene";
 import { UiManager } from "../ui/ui.manager";
 import { LoadingScene } from "./loading.scene";
-import { SceneName } from "structure";
+import { ModuleName, SceneName } from "structure";
 
 export class CreateRoleScene extends BasicScene {
   private params: any;
   constructor() {
-    super({ key: SceneName.CREATE_ROLE_SCENE});
+    super({ key: SceneName.CREATE_ROLE_SCENE });
   }
 
   public init(data?: any) {
@@ -19,7 +19,7 @@ export class CreateRoleScene extends BasicScene {
     if (this.render) {
       const uimanager: UiManager = this.render.uiManager;
       uimanager.setScene(this);
-      uimanager.showPanel("CreateRole", this.params);
+      uimanager.showPanel(ModuleName.CREATEROLE_NAME, this.params);
       this.render.sceneManager.sleepScene(LoadingScene.name);
     }
   }
