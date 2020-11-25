a0efe8cf699058e200b19a467c589267dfde84cf
WannaTwo <33625108+WannaTwo-WeiZhou@users.noreply.github.com>
2020-11-25 17:19:20 +0800

    修复主界面右上角钻石购买按钮

--- src/pica/render/ui/PicaMainUI/PicaMainUIPanel.ts
+++ src/pica/render/ui/PicaMainUI/PicaMainUIPanel.ts
@@ -232,6 +232,7 @@ export class PicaMainUIPanel extends BasePanel {
         this.add(this.playerCon);
         this.mCoinValue = new ValueContainer(this.scene, this.key, "coin", this.dpr);
         this.mDiamondValue = new ValueContainer(this.scene, this.key, "diamond", this.dpr);
+        this.mDiamondValue.setHandler(new Handler(this, this.onOpenRechargeHandler));
         this.mDiamondValue.y = 35 * this.dpr;
 
         const frame = this.scene.textures.getFrame(this.key, "strength_progress");
@@ -300,7 +301,7 @@ export class PicaMainUIPanel extends BasePanel {
         this.render.renderEmitter("openroompanel");
     }
     private onHeadHandler() {
-        this.render.renderEmitter("showPanel",ModuleName.CHARACTERINFO_NAME);
+        this.render.renderEmitter("showPanel", ModuleName.CHARACTERINFO_NAME);
     }
 
     private onStrengthHandler() {
@@ -352,32 +353,38 @@ export class PicaMainUIPanel extends BasePanel {
     private onPartyHandler() {
         this.render.renderEmitter("showPanel", ModuleName.PICAOPENPARTY_NAME);
     }
+    private onOpenRechargeHandler() {
+        this.render.renderEmitter("showPanel", ModuleName.PICARECHARGE_NAME);
+    }
 }
 
 class ValueContainer extends Phaser.GameObjects.Container {
     protected mText: Phaser.GameObjects.Text;
-    protected mAddBtn: Phaser.GameObjects.Image;
+    protected mAddBtn: Button;
     protected mLeft: Phaser.GameObjects.Image;
+    protected sendHandler: Handler;
     constructor(scene: Phaser.Scene, key: string, leftIcon: string, dpr: number = 1) {
         super(scene);
         this.init(key, leftIcon, dpr);
     }
-
     public setText(val: string) {
         this.mText.setText(val);
     }
 
+    public setHandler(send: Handler) {
+        this.sendHandler = send;
+    }
     protected init(key: string, leftIcon: string, dpr: number) {
         const bg = this.scene.make.image({
             key,
             frame: "home_progress_bottom"
         }, false);
-
+        bg.texture.setFilter(Phaser.Textures.FilterMode.LINEAR);
         this.mLeft = this.scene.make.image({
             key,
             frame: leftIcon,
         }, false);
-
+        this.mLeft.texture.setFilter(Phaser.Textures.FilterMode.LINEAR);
         this.mText = this.scene.make.text({
             text: "0",
             width: bg.width,
@@ -388,19 +395,20 @@ class ValueContainer extends Phaser.GameObjects.Container {
             }
         }, false).setOrigin(0.5);
         this.mText.setStroke("#000000", 1 * dpr);
-
-        this.mAddBtn = this.scene.make.image({
-            key,
-            frame: "add_btn"
-        });
+        this.mText.texture.setFilter(Phaser.Textures.FilterMode.LINEAR);
+        this.mAddBtn = new Button(this.scene, key, "add_btn", "add_btn");
         this.setSize(bg.width, bg.height);
-        this.mLeft.x = -bg.width * 0.5;
+        this.mLeft.x = -bg.width * 0.5 + 5 * dpr;
         this.mLeft.y = bg.y + bg.height * 0.5 - this.mLeft.height * 0.5;
         this.mAddBtn.x = this.width * this.originX - this.mAddBtn.width * this.originX - 5 * dpr;
-        this.mText.x = 10 * dpr;
+        this.mText.x = 4 * dpr;
         this.add([bg, this.mLeft, this.mText, this.mAddBtn]);
+        this.mAddBtn.on(ClickEvent.Tap, this.onAddHandler, this);
     }
 
+    private onAddHandler() {
+        if (this.sendHandler) this.sendHandler.run();
+    }
 }
 
 class IconText extends Phaser.GameObjects.Container {
@@ -553,13 +561,14 @@ class ProgressValue extends ValueContainer {
             key,
             frame: "home_progress_bottom"
         }, false);
+        bg.texture.setFilter(Phaser.Textures.FilterMode.LINEAR);
         this.setSize(bg.width, bg.height);
 
         this.mLeft = this.scene.make.image({
             key,
             frame: leftIcon,
         }, false);
-
+        this.mLeft.texture.setFilter(Phaser.Textures.FilterMode.LINEAR);
         this.mProgress = new ProgressBar(this.scene, dpr);
         this.mProgress.x = -this.width / 2 + 10 * dpr;
         this.mProgress.y = 4 * dpr;
@@ -574,18 +583,14 @@ class ProgressValue extends ValueContainer {
             }
         }, false).setOrigin(0.5);
         this.mText.setStroke("#000000", 1 * dpr);
-
-        this.mAddBtn = this.scene.make.image({
-            key,
-            frame: "add_btn"
-        });
+        this.mText.texture.setFilter(Phaser.Textures.FilterMode.LINEAR);
+        this.mAddBtn = new Button(this.scene, key, "add_btn", "add_btn");
         this.setSize(bg.width, bg.height);
         this.mLeft.x = -this.width * this.originX + 13 * dpr;
         this.mAddBtn.x = this.width * this.originX - 10 * dpr;
         //  this.mText.y = (this.height - this.mText.height) / 2;
         this.add([bg, this.mProgress, this.mLeft, this.mText, this.mAddBtn]);
     }
-
 }
 
 class ProgressBar extends Phaser.GameObjects.Container {
