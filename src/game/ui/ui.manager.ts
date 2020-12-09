ff4bd4872997d586bc1da044e96479e53d26384a
liumeng <liumeng@apowo.com>
2020-12-09 19:40:23 +0800

    修改

--- src/game/ui/ui.manager.ts
+++ src/game/ui/ui.manager.ts
@@ -293,14 +293,14 @@ export class UIManager extends PacketHandler {
         this.showMed(ModuleName.CREATEROLE_NAME, packet.content);
     }
 
-    protected onForceOfflineHandler(packet: PBpacket) {
-        // const alert = new AlertView(this.mScene, this.worldService).show({
-        //     text: i18n.t("common.offline"),
-        //     callback: () => {
-        //     },
-        //     btns: Buttons.Ok
-        // });
-    }
+    // protected onForceOfflineHandler(packet: PBpacket) {
+    //     // const alert = new AlertView(this.mScene, this.worldService).show({
+    //     //     text: i18n.t("common.offline"),
+    //     //     callback: () => {
+    //     //     },
+    //     //     btns: Buttons.Ok
+    //     // });
+    // }
 
     protected onHandleCloseCreateRoleUI() {
         this.hideMed(ModuleName.CREATEROLE_NAME);
