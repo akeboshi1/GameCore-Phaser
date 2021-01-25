6834e702f277b94f3f4b06b6af2a383715e63bcb
zhangteng <zhangteng@apowo.com>
2021-01-25 19:45:16 +0800

    v1.0.489

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ const config = {
         },
     },
     optimization: {
-        minimize: false,
+        minimize: true,
         minimizer: [
             new TerserPlugin({
                 sourceMap: true,
