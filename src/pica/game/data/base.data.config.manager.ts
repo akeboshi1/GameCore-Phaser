245759ed57acbe839ddc253ad1900fe0e7d97661
liumeng <liumeng@apowo.com>
2021-02-07 15:07:31 +0800

    背包配置

--- src/pica/game/data/base.data.config.manager.ts
+++ src/pica/game/data/base.data.config.manager.ts
@@ -46,21 +46,23 @@ export class BaseDataConfigManager extends BaseConfigManager {
             item.source = this.getI18n(item.source, { id: item.id, source: "source" });
             item.des = this.getI18n(item.des, { id: item.id, des: "des" });
             item["exclude"] = data.excludes;
+            if (item.texturePath) item["display"] = { texturePath: item.texturePath };
             if (item.elementId && item.elementId !== "") {
                 const element = this.getElementData(item.elementId);
-                const texture_path = element.texture_path;
                 if (element) {
+                    const texture_path = element.texture_path;
                     item["animations"] = element["AnimationData"];
-                    if (element.data_path || texture_path)
+                    if (texture_path) {
                         item["animationDisplay"] = { dataPath: element.data_path, texturePath: texture_path };
-                }
-                const index = texture_path.lastIndexOf(".");
-                if (index === -1) {
-                    item.texturePath = element.texture_path + "_s";
-                } else {
-                    const extensions = texture_path.slice(index, texture_path.length);
-                    const path = texture_path.slice(0, index);
-                    item.texturePath = path + "_s" + extensions;
+                        const index = texture_path.lastIndexOf(".");
+                        if (index === -1) {
+                            item.texturePath = element.texture_path + "_s";
+                        } else {
+                            const extensions = texture_path.slice(index, texture_path.length);
+                            const path = texture_path.slice(0, index);
+                            item.texturePath = path + "_s" + extensions;
+                        }
+                    }
                 }
             }
             item["find"] = true;
