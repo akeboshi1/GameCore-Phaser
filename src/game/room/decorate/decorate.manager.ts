64fa8966bbbfcd4ea8245f3af8e3941ed7b7f3e7
WannaTwo <33625108+WannaTwo-WeiZhou@users.noreply.github.com>
2021-02-24 10:03:50 +0800

    add decorate manager

--- src/game/room/decorate/decorate.manager.ts
+++ src/game/room/decorate/decorate.manager.ts
@@ -3,6 +3,8 @@ import {op_client} from "pixelpai_proto";
 
 export class DecorateManager {
 
+    private queue: DecorateAction[] = [];
+
     constructor(private mRoom: Room) {
     }
 
@@ -15,6 +17,28 @@ export class DecorateManager {
     }
 
     public reset() {
+
+    }
+}
+
+export enum DecorateActionType {
+    Add,// 添加
+    Remove,// 回收
+    Move,// 移动
+    Rotate// 旋转
+}
+
+class DecorateAction {
+    constructor(public target: number, public type: DecorateActionType, public data?: any) {
+    }
+
+    // 执行
+    public execute() {
+
+    }
+
+    // 撤销
+    public resolve() {
 
     }
 }
