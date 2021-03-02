bc30801da7442132e6f619b2b8f19d0f7c0ca8ad
guanxiaomin <guanxiaomin@apowo.com>
2021-03-02 11:16:21 +0800

    回退版本1.0.527

--- src/structure/worker.name.ts
+++ src/structure/worker.name.ts
@@ -6,7 +6,7 @@ export const PHYSICAL_WORKER = "physicalPeer";
 // export const HEARTBEAT_WORKER_URL = "js/heartWorker.js";
 import version from "../../version";
 // webworker_rpc中直接使用的传入路径  支持相对路径
-export const MAIN_WORKER_URL = "../js/mainWorker_v" + version + ".js";
-export const PHYSICAL_WORKER_URL = "../js/physicalWorker_v" + version + ".js";
+export const MAIN_WORKER_URL = "js/mainWorker_v" + version + ".js";
+export const PHYSICAL_WORKER_URL = "js/physicalWorker_v" + version + ".js";
 // export const MAIN_WORKER_URL = "js/mainWorker.js";
 // export const PHYSICAL_WORKER_URL = "js/physicalWorker.js";
