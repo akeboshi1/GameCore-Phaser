6b238b284f3fc529d2c44751288131e882f5ec0a
liumeng <liumeng@apowo.com>
2021-03-04 11:19:26 +0800

    注释最后一个关卡完全场景

--- src/pica/render/ui/PicaExploreList/PicaExploreListLevelPanel.ts
+++ src/pica/render/ui/PicaExploreList/PicaExploreListLevelPanel.ts
@@ -200,11 +200,11 @@ export class PicaExploreListLevelPanel extends Phaser.GameObjects.Container {
     }
 
     private onChapterLevelHandler(data: op_client.IPKT_EXPLORE_LEVEL_DATA) {
-        let roomid = data.roomId;
-        if (data.progress === 500) {
-            const tempid = data["completeScene"];
-            roomid = tempid || roomid;
-        }
+        const roomid = data.roomId;
+        // if (data.progress === 500) {
+        //     const tempid = data["completeScene"];
+        //     roomid = tempid || roomid;
+        // }
         if (this.send) this.send.runWith(["roomid", roomid]);
     }
 
