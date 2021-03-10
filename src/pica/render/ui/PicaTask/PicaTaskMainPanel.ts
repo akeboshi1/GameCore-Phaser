f46692fb28be402fcf0926a08c5a55266e3c6966
liumeng <liumeng@apowo.com>
2021-03-10 15:03:41 +0800

    任务动画

--- src/pica/render/ui/PicaTask/PicaTaskMainPanel.ts
+++ src/pica/render/ui/PicaTask/PicaTaskMainPanel.ts
@@ -39,6 +39,7 @@ export class PicaTaskMainPanel extends Phaser.GameObjects.Container {
     }
     setTaskDatas(content: op_client.OP_VIRTUAL_WORLD_RES_CLIENT_PKT_QUERY_QUEST_GROUP, questType: op_pkt_def.PKT_Quest_Type) {
         this.gameScroller.clearItems(false);
+        if (this.mainTaskAnimation) this.mainTaskAnimation.dispose();
         if (!content.id) {
             // console.log("没有新任务了");
             this.notaskTip.visible = true;
@@ -54,6 +55,8 @@ export class PicaTaskMainPanel extends Phaser.GameObjects.Container {
         }
         this.mainItem.setMainTaskData(content, questType);
         this.gameScroller.addItem(this.mainItem);
+
+        if (this.taskGroupData) this.questType = questType;
         if (this.questType !== questType) {
             const tempArr = this.getTaskQuests(content.quests, undefined);
             this.setTaskItems(tempArr);
@@ -69,6 +72,7 @@ export class PicaTaskMainPanel extends Phaser.GameObjects.Container {
             for (const item of this.taskItems) {
                 if (item.visible) tempitems.push(item);
             }
+            this.isFinishGroup = true;
             if (this.isFinishGroup) {
                 this.mainTaskAnimation = new MainTaskAnimation(this.scene, this.mainItem, tempitems, this.width, this.dpr);
                 if (this.isMoveFinish) this.mainTaskAnimation.playIntoAnimation();
@@ -399,6 +403,7 @@ class MainTaskAnimation {
     private taskItems: PicaTaskItem[];
     private listPosY = [];
     private tempItems = [];
+    private itemTweens: Phaser.Tweens.Tween[] = [];
     private width: number;
     private dpr: number;
     private isDispose: boolean = false;
@@ -422,8 +427,14 @@ class MainTaskAnimation {
     }
 
     public dispose() {
+        if (this.isDispose) return;
+        for (const tween of this.itemTweens) {
+            tween.stop();
+            tween.remove();
+        }
         this.tempItems.length = 0;
         this.taskItems.length = 0;
+        this.itemTweens.length = 0;
         this.mainItem = undefined;
         this.taskItems = undefined;
         this.tempItems = undefined;
@@ -466,6 +477,7 @@ class MainTaskAnimation {
             onComplete: () => {
                 tween.stop();
                 tween.remove();
+                this.removeTween(tween);
                 if (moveY) {
                     if (this.indexed > 0)
                         this.playMoveY();
@@ -473,6 +485,7 @@ class MainTaskAnimation {
                 }
             },
         });
+        this.itemTweens.push(tween);
     }
 
     private playAlpha(target, from, to, ease: string) {
@@ -488,8 +501,10 @@ class MainTaskAnimation {
             onComplete: () => {
                 tween.stop();
                 tween.remove();
+                this.removeTween(tween);
             },
         });
+        this.itemTweens.push(tween);
     }
 
     private playMoveY() {
@@ -512,9 +527,18 @@ class MainTaskAnimation {
             onComplete: () => {
                 tween.stop();
                 tween.remove();
+                this.removeTween(tween);
                 this.playNextAnimation();
             },
         });
+        this.itemTweens.push(tween);
+    }
+    private removeTween(tween: Phaser.Tweens.Tween) {
+        if (this.isDispose) return;
+        const index = this.itemTweens.indexOf(tween);
+        if (index !== -1) {
+            this.itemTweens.splice(index, 1);
+        }
     }
 }
 
@@ -526,6 +550,7 @@ class MainTaskGooutAnimation {
     private dpr: number;
     private isDispose: boolean = false;
     private scene: Phaser.Scene;
+    private itemTweens: Phaser.Tweens.Tween[] = [];
     constructor(scene: Phaser.Scene, taskItems: PicaTaskItem[], width: number, dpr: number) {
         this.scene = scene;
         this.taskItems = taskItems;
@@ -565,8 +590,14 @@ class MainTaskGooutAnimation {
     }
 
     public dispose() {
+        if (this.isDispose) return;
+        for (const tween of this.itemTweens) {
+            tween.stop();
+            tween.remove();
+        }
         this.tempItems.length = 0;
         this.taskItems.length = 0;
+        this.itemTweens.length = 0;
         this.taskItems = undefined;
         this.tempItems = undefined;
         this.isDispose = true;
@@ -590,12 +621,14 @@ class MainTaskGooutAnimation {
             onComplete: () => {
                 tween.stop();
                 tween.remove();
+                this.removeTween(tween);
                 if (!this.scene) return;
                 for (const item of target) {
                     item.visible = false;
                 }
             },
         });
+        this.itemTweens.push(tween);
     }
 
     private playAlpha(target, from, to, ease: string) {
@@ -611,8 +644,10 @@ class MainTaskGooutAnimation {
             onComplete: () => {
                 tween.stop();
                 tween.remove();
+                this.removeTween(tween);
             },
         });
+        this.itemTweens.push(tween);
     }
 
     private playMoveGooutY(tempArr: PicaTaskItem[], indexed: number, delay: number) {
@@ -636,10 +671,21 @@ class MainTaskGooutAnimation {
             onComplete: () => {
                 tween.stop();
                 tween.remove();
+                this.removeTween(tween);
                 if (!this.isDispose) {
                     this.dispose();
                 }
             },
         });
+
+        this.itemTweens.push(tween);
+    }
+
+    private removeTween(tween: Phaser.Tweens.Tween) {
+        if (this.isDispose) return;
+        const index = this.itemTweens.indexOf(tween);
+        if (index !== -1) {
+            this.itemTweens.splice(index, 1);
+        }
     }
 }
