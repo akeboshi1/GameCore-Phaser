596d97b172fb1a3d996843c37883bb95b925b11b
guanxiaomin <guanxiaomin@apowo.com>
2021-06-17 09:21:41 +0800

    update modular code

--- src/gamecore/render/render.ts
+++ src/gamecore/render/render.ts
@@ -283,10 +283,6 @@ export class Render extends RPCPeer implements GameMain, IRender {
         //     this.mLocalStorageManager.destroy();
         //     this.mLocalStorageManager = undefined;
         // }
-        if (this.mSceneManager) {
-            this.mSceneManager.destroy();
-            this.mSceneManager = undefined;
-        }
         if (this.mSoundManager) {
             this.mSoundManager.destroy();
             this.mSoundManager = undefined;
@@ -306,6 +302,10 @@ export class Render extends RPCPeer implements GameMain, IRender {
         if (this.mEditorCanvasManager) {
             this.mEditorCanvasManager.destroy();
             this.mEditorCanvasManager = undefined;
+        }
+        if (this.mSceneManager) {
+            this.mSceneManager.destroy();
+            this.mSceneManager = undefined;
         }
     }
 
