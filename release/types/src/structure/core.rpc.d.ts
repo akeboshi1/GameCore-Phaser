7d0612c1d11f78bcd22f851a5e3a4f3dbefe5ec9
guanxiaomin <guanxiaomin@apowo.com>
2021-06-23 10:10:45 +0800

    add corerpc structure

---
+++ release/types/src/structure/core.rpc.d.ts
@@ -0,0 +1,9 @@
+export declare const CoreExport: () => (target: any, name?: any, descriptor?: any) => void;
+export declare enum CoreParamType {
+    str = 1,
+    boolean = 2,
+    num = 3,
+    unit8array = 4,
+    executor = 5,
+    custom = 6
+}
