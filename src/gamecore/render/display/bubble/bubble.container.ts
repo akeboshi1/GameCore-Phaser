b53cc57ac5b99c3dc8ddc948ce961b00f44f74ce
guanxiaomin <guanxiaomin@apowo.com>
2021-06-24 14:03:11 +0800

    remove bubble.container destroy child

--- src/gamecore/render/display/bubble/bubble.container.ts
+++ src/gamecore/render/display/bubble/bubble.container.ts
@@ -50,13 +50,13 @@ export class BubbleContainer extends Phaser.GameObjects.Container {
 
     public destroy(fromScene?: boolean): void {
         if (!this.mBubbles) return;
-        const len = this.mBubbles.length;
-        let bul: Bubble = null;
-        for (let i = len - 1; i >= 0; i--) {
-            bul = this.mBubbles[i];
-            if (!bul) continue;
-            bul.destroy();
-        }
+        // const len = this.mBubbles.length;
+        // let bul: Bubble = null;
+        // for (let i = len - 1; i >= 0; i--) {
+        //     bul = this.mBubbles[i];
+        //     if (!bul) continue;
+        //     bul.destroy();
+        // }
         this.mBubbles = null;
         this.removeFormParent();
         super.destroy(fromScene);
