d996d1013fae1be9a2e0509586480a1567fe6f29
guanxiaomin <guanxiaomin@apowo.com>
2021-06-29 10:42:45 +0800

    update account rpc code

--- src/gamecore/render/account/account.ts
+++ src/gamecore/render/account/account.ts
@@ -1,4 +1,5 @@
 import { IPos, Logger } from "structure";
+import { Export } from "webworker-rpc";
 
 interface IAccountData {
     accessToken: string;
@@ -21,7 +22,7 @@ export class Account {
         // 1. 登陆注册的逻辑在这里做
         // 2. 缓存用户登陆后的帐号咨讯
     }
-
+    @Export()
     public getAccountData() {
         return this.accountData;
     }
@@ -31,7 +32,7 @@ export class Account {
     //         resolve(this.accountData);
     //     });
     // }
-
+    @Export()
     public setAccount(val: any) {
         // this.clear();
         // Object.assign(this.mCurAccountData, val);
@@ -45,7 +46,7 @@ export class Account {
         };
         this.saveLocalStorage();
     }
-
+    @Export()
     public refreshToken(data: any) {
         if (!this.accountData) {
             try {
@@ -60,7 +61,7 @@ export class Account {
         this.accountData.accessToken = newToken;
         this.saveLocalStorage();
     }
-
+    @Export()
     public saveLocalStorage() {
         if (!this.accountData) {
             return;
@@ -72,17 +73,17 @@ export class Account {
             Logger.getInstance().warn("write localStorage fail");
         }
     }
-
+    @Export()
     public clear() {
         this.accountData = null;
     }
-
+    @Export()
     public destroy() {
         this.clear();
         localStorage.removeItem("token");
         this.enterGame(undefined, undefined, undefined, undefined, undefined);
     }
-
+    @Export()
     public enterGame(gameId: string, virtualWorldId: string, sceneId: number, loc: any, spawnPointId, worldId?: string) {
         this.gameId = gameId;
         this.virtualWorldId = virtualWorldId;
